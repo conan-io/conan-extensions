f73a9433005f86da21b126c55d5cab74a5c9bb36
Rubén Rincón Blanco <rubenrb@jfrog.com>
2023-02-27 22:26:12 +0100

    Add conanfile.txt to conanfile.py converter

---
+++ command-extensions/extensions/commands/conan-extensions/cmd_convert_txt_recipe.py
@@ -0,0 +1,71 @@
+import os
+import textwrap
+from jinja2 import Template
+from conan.cli.command import conan_command
+from conan.api.output import cli_out_write
+from conans.client.loader_txt import ConanFileTextLoader
+
+
+@conan_command(group="Custom commands", formatters={"text": cli_out_write})
+def convert_txt_recipe(conan_api, parser, *args, **kwargs):
+    """
+    Convert a conanfile.txt to a conanfile.py
+    """
+    parser.add_argument("path", help="Path to a folder containing a conanfile.txt")
+    args = parser.parse_args(*args)
+
+    path = conan_api.local.get_conanfile_path(args.path, os.getcwd(), py=False)
+    txt = ConanFileTextLoader(open(path, "r").read())
+    template = textwrap.dedent('''
+        from conan import ConanFile
+        {% if layout == "cmake_layout" %}
+        from conan.tools.cmake import cmake_layout
+        {% endif %}
+        {% if layout == "vs_layout" %}
+        from conan.tools.microsoft import vs_layout
+        {% endif %}
+
+        class Pkg(ConanFile):
+            {% if generators %}
+            generators ={% for g in generators %} "{{g}}",{%endfor%}
+            {% endif %}
+            {% if options %}
+            default_options = {{options}}
+            {% endif %}
+
+            def requirements(self):
+                {% for r in requires %}
+                self.requires("{{r}}")
+                {% endfor %}
+                {% if not requires %}
+                pass
+                {% endif %}
+
+            def build_requirements(self):
+                {% for r in test_requires %}
+                self.test_requires("{{r}}")
+                {% endfor %}
+                {% for r in tool_requires %}
+                self.tool_requires("{{r}}")
+                {% endfor %}
+                {% if not tool_requires and not test_requires%}
+                pass
+                {% endif %}
+
+            {% if layout %}
+            def layout(self):
+                {{layout}}(self)
+            {% endif %}
+            ''')
+    conanfile = Template(template, trim_blocks=True, lstrip_blocks=True)
+    options = {}
+    for o in txt.options.splitlines():
+        k, v = o.split("=")
+        options[k] = v
+    conanfile = conanfile.render({"requires": txt.requirements,
+                                  "tool_requires": txt.tool_requirements,
+                                  "test_requires": txt.test_requirements,
+                                  "generators": txt.generators,
+                                  "options": options,
+                                  "layout": txt.layout})
+    return conanfile
