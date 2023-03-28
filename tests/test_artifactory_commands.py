a01625e43d76178c4cc2fcf03630b6c5c2c3289d
czoido <mrgalleta@gmail.com>
2023-03-28 14:25:17 +0200

    wip

--- tests/test_artifactory_commands.py
+++ tests/test_artifactory_commands.py
@@ -38,11 +38,14 @@ def run(cmd):
 def test_build_info_create():
     repo = os.path.join(os.path.dirname(__file__), "..")
 
+    build_name = "test_bi_create"
+    build_number = "1"
+
     run(f"conan config install {repo}")
     run("conan new cmake_lib -d name=mypkg -d version=1.0 --force")
     run("conan create . --format json > create.json")
     run("conan remove mypkg -c -r extensions-stg")
     run("conan upload mypkg/1.0 -c -r extensions-stg")
-    run("conan art:build-info create create.json test_bi_create 1 > buildinfo.json")
-    run(f'conan art:property set https://conanv2beta.jfrog.io/artifactory extensions-stg mypkg/1.0 --property="build.name=test_bi_create" --property="build.number=1" --user="{os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")}" --password="{os.getenv("CONAN_PASSWORD_EXTENSIONS_STG")}"')
-    run(f'conan art:build-info upload buildinfo.json https://conanv2beta.jfrog.io/artifactory --user="{os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")}" --password="{os.getenv("CONAN_PASSWORD_EXTENSIONS_STG")}"')
+    run(f'conan art:property set https://conanv2beta.jfrog.io/artifactory extensions-stg mypkg/1.0 --property="build.name={build_name}" --property="build.number={build_number}" --user="{os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")}" --password="{os.getenv("CONAN_PASSWORD_EXTENSIONS_STG")}"')
+    run(f'conan art:build-info create create.json {build_name} {build_number} > {build_name}.json')
+    run(f'conan art:build-info upload {build_name}.json https://conanv2beta.jfrog.io/artifactory --user="{os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")}" --password="{os.getenv("CONAN_PASSWORD_EXTENSIONS_STG")}"')
