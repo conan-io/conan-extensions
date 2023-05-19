39bc90256d900f3bd68ab2513b077d2262f3e02f
danimtb <danimanzaneque@gmail.com>
2023-05-19 11:52:51 +0200

    rename remote to server

--- tests/test_artifactory_commands.py
+++ tests/test_artifactory_commands.py
@@ -215,72 +215,72 @@ def test_fail_if_not_uploaded():
 
 
 @pytest.mark.requires_credentials
-def test_remote_complete():
+def test_server_complete():
     """
-    Test remote add, list, remove commands
+    Test server add, list, remove commands
     """
 
     repo = os.path.join(os.path.dirname(__file__), "..")
     run(f"conan config install {repo}")
-    remote_url = os.getenv("ART_URL")
-    remote_user = os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")
+    server_url = os.getenv("ART_URL")
+    server_user = os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")
 
-    out_add = run(f'conan art:remote add remote1 {remote_url} --user {remote_user} '
+    out_add = run(f'conan art:server add server1 {server_url} --user {server_user} '
                   f'--password {os.getenv("CONAN_PASSWORD_EXTENSIONS_STG")}')
 
-    assert f"Remote 'remote1' ({remote_url}) added successfully" in out_add
-    assert os.path.exists(os.path.join(os.path.dirname(__file__), "remotes.json"))
+    assert f"Remote 'server1' ({server_url}) added successfully" in out_add
+    assert os.path.exists(os.path.join(os.path.dirname(__file__), ".art-servers"))
 
-    out_list = run('conan art:remote list')
+    out_list = run('conan art:server list')
 
-    assert "remote1:" in out_list
-    assert f"url: {remote_url}" in out_list
-    assert f"user: {remote_user}" in out_list
+    assert "server1:" in out_list
+    assert f"url: {server_url}" in out_list
+    assert f"user: {server_user}" in out_list
     assert "password: ***" in out_list
 
-    out_remove = run('conan art:remote remove remote1')
+    out_remove = run('conan art:server remove server1')
 
-    assert f"Remote 'remote1' ({remote_url}) removed successfully" in out_remove
+    assert f"Remote 'server1' ({server_url}) removed successfully" in out_remove
 
 
 @pytest.mark.requires_credentials
-def test_remote_add_error():
+def test_server_add_error():
     """
-    Test remote add error when adding a remote with same name
+    Test server add error when adding a server with same name
     """
     repo = os.path.join(os.path.dirname(__file__), "..")
     run(f"conan config install {repo}")
-    remote_url = os.getenv("ART_URL")
-    remote_user = os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")
+    server_url = os.getenv("ART_URL")
+    server_user = os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")
 
-    run(f'conan art:remote add remote1 {remote_url} --user {remote_user} '
+    run(f'conan art:server add server1 {server_url} --user {server_user} '
         f'--password {os.getenv("CONAN_PASSWORD_EXTENSIONS_STG")}')
-    out_add = run(f'conan art:remote add remote1 other_url --user other_user --password other_pass')
+    out_add = run(f'conan art:server add server1 other_url --user other_user --password other_pass')
 
-    assert f"Remote 'remote1' ({remote_url}) already exist." in out_add
+    assert f"Remote 'server1' ({server_url}) already exist." in out_add
 
 
-def test_remote_remove_error():
+def test_server_remove_error():
     """
-    Test remote remove errors when there is no remote with the provided name
-    """
-
-    repo = os.path.join(os.path.dirname(__file__), "..")
-    run(f"conan config install {repo}")
-
-    out = run("conan art:remote remove remote1", error=True)
-
-    assert "Remote 'remote1' does not exist." in out
-
-
-def test_remote_list_empty():
-    """
-    Test remote list output when no remotes are configured
+    Test server remove errors when there is no server with the provided name
     """
 
     repo = os.path.join(os.path.dirname(__file__), "..")
     run(f"conan config install {repo}")
 
-    out = run("conan art:remote list")
+    out = run("conan art:server remove server1", error=True)
 
-    assert "No remotes configured. Use `conan art:remote add` command to add one." in out
+    assert "Remote 'server1' does not exist." in out
+
+
+def test_server_list_empty():
+    """
+    Test server list output when no servers are configured
+    """
+
+    repo = os.path.join(os.path.dirname(__file__), "..")
+    run(f"conan config install {repo}")
+
+    out = run("conan art:server list")
+
+    assert "No servers configured. Use `conan art:server add` command to add one." in out
