31fd6ededfb8bd2b0d8bfc5dd8680e894ca0b003
danimtb <danimanzaneque@gmail.com>
2023-05-24 13:50:13 +0200

    brake circular dependency

--- extensions/commands/art/cmd_server.py
+++ extensions/commands/art/cmd_server.py
@@ -8,9 +8,55 @@ from conan.api.output import ConanOutput, cli_out_write
 from conan.cli.command import conan_command, conan_subcommand
 from conan.errors import ConanException
 
-from cmd_build_info import api_request
-
 SERVERS_FILENAME = ".art-servers"
+
+
+def response_to_str(response):
+    content = response.content
+    try:
+        # A bytes message, decode it as str
+        if isinstance(content, bytes):
+            content = content.decode('utf-8')
+
+        content_type = response.headers.get("content-type")
+
+        if content_type == "application/json":
+            # Errors from Artifactory looks like:
+            #  {"errors" : [ {"status" : 400, "message" : "Bla bla bla"}]}
+            try:
+                data = json.loads(content)["errors"][0]
+                content = "{}: {}".format(data["status"], data["message"])
+            except Exception:
+                pass
+        elif "text/html" in content_type:
+            content = "{}: {}".format(response.status_code, response.reason)
+
+        return content
+    except Exception:
+        return response.content
+
+
+def api_request(method, request_url, user=None, password=None, json_data=None,
+                sign_key_name=None):
+    headers = {}
+    if json_data:
+        headers.update({"Content-Type": "application/json"})
+    if sign_key_name:
+        headers.update({"X-JFrog-Crypto-Key-Name": sign_key_name})
+
+    requests_method = getattr(requests, method)
+    if user and password:
+        response = requests_method(request_url, auth=(
+            user, password), data=json_data, headers=headers)
+    else:
+        response = requests_method(request_url)
+
+    if response.status_code == 401:
+        raise Exception(response_to_str(response))
+    elif response.status_code not in [200, 204]:
+        raise Exception(response_to_str(response))
+
+    return response_to_str(response)
 
 
 def get_server(server_name):
