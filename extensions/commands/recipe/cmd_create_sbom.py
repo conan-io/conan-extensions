146ff481108a4d4c459114d777f72c6e7faefdc7
Ivo Hedtke <ivo.hedtke@dbschenker.com>
2023-08-08 14:44:18 +0200

    Use conan_api instead of parsing the graph JSON

--- extensions/commands/recipe/cmd_create_sbom.py
+++ extensions/commands/recipe/cmd_create_sbom.py
@@ -1,4 +1,5 @@
-import json
+import os.path
+
 from packageurl import PackageURL
 from cyclonedx.factory.license import LicenseFactory
 from cyclonedx.model import ExternalReference, ExternalReferenceType, LicenseChoice, XsUri
@@ -8,8 +9,9 @@ from cyclonedx.output.json import JsonV1Dot4
 
 from conan.api.output import cli_out_write
 from conan.api.conan_api import ConanAPI
+from conan.cli.args import common_graph_args, validate_common_graph_args
 from conan.cli.command import conan_command
-
+from conans.client.graph.graph import Node
 
 lFac = LicenseFactory()
 
@@ -21,52 +23,48 @@ def package_type_to_component_type(pt: str) -> ComponentType:
         return ComponentType.LIBRARY
 
 
-def licenses(id):
+def licenses(ids):
     """
     see https://cyclonedx.org/docs/1.4/json/#components_items_licenses
     """
-    if id is None:
+    if ids is None:
         return None
-    else:
-        return [LicenseChoice(license=lFac.make_from_string(id))]
+    if not isinstance(ids, tuple):
+        ids = [ids]
+    return [LicenseChoice(license=lFac.make_from_string(i)) for i in ids]
 
 
-def package_url(node: dict) -> PackageURL:
+def package_url(node: Node) -> PackageURL:
     """
     Creates a PURL following https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst#conan
     """
-    qualifiers = {
-        "prev": node["prev"]
-    }
-    if node["user"]:
-        qualifiers["user"] = node["user"]
-    if node["channel"]:
-        qualifiers["channel"] = node["channel"]
-    if "#" in node["ref"]:
-        qualifiers["rref"] = node["ref"].split("#")[1]
-    if node["remote"]:  # "https://center.conan.io" is default can be omitted
-        qualifiers["repository_url"] = node["remote"]
     return PackageURL(
         type="conan",
-        name=node["name"],
-        version=node["version"],
-        qualifiers=qualifiers)
+        name=node.conanfile.name,
+        version=node.conanfile.version,
+        qualifiers={
+            "prev": node.prev,
+            "rref": node.ref.revision,
+            "user": node.conanfile.user,
+            "channel": node.conanfile.channel,
+            "repository_url": node.remote
+        })
 
 
-def create_component(n):
+def create_component(n: Node) -> Component:
     result = Component(
-        type=package_type_to_component_type(n["package_type"]),
-        name=n["name"],
-        version=n["version"],
-        licenses=licenses(n["license"]),
+        type=package_type_to_component_type(n.conanfile.package_type),
+        name=n.conanfile.name,
+        version=n.conanfile.version,
+        licenses=licenses(n.conanfile.license),
         bom_ref=package_url(n).to_string(),
         purl=package_url(n),
-        description=n["description"]
+        description=n.conanfile.description
     )
-    if n["homepage"]:
+    if n.conanfile.homepage:
         result.external_references.add(ExternalReference(
             type=ExternalReferenceType.WEBSITE,
-            url=XsUri(n["homepage"]),
+            url=XsUri(n.conanfile.homepage),
         ))
     return result
 
@@ -76,19 +74,37 @@ def create_sbom(conan_api: ConanAPI, parser, *args):
     """
     creates an SBOM in CycloneDX 1.4 JSON format from a Conan graph JSON
     """
-    parser.add_argument("graph_json", help="Path to Conan generated graph JSON output file.")
-    args = parser.parse_args(*args)
-    with open(args.graph_json, 'r') as f:
-        data = json.load(f)
-    deps = data["graph"]["nodes"]
 
-    root = deps.pop("0")
+    # BEGIN COPY FROM conan: cli/commands/graph.py
+    common_graph_args(parser)
+    args = parser.parse_args(*args)
+    validate_common_graph_args(args)
+    cwd = os.getcwd()
+    path = conan_api.local.get_conanfile_path(args.path, cwd, py=None) if args.path else None
+    remotes = conan_api.remotes.list(args.remote) if not args.no_remote else []
+    overrides = eval(args.lockfile_overrides) if args.lockfile_overrides else None
+    lockfile = conan_api.lockfile.get_lockfile(lockfile=args.lockfile,
+                                               conanfile_path=path,
+                                               cwd=cwd,
+                                               partial=args.lockfile_partial,
+                                               overrides=overrides)
+    profile_host, profile_build = conan_api.profiles.get_profiles_from_args(args)
+    deps_graph = conan_api.graph.load_graph_consumer(path, args.name, args.version,
+                                                     args.user, args.channel,
+                                                     profile_host, profile_build, lockfile,
+                                                     remotes, args.update)
+    # END COPY
+    root = deps_graph.root
     root_component = create_component(root)
     bom = Bom()
     bom.metadata.component = root_component
-    for n in deps.values():
+    component_per_node = {root: root_component}
+    for n in deps_graph.nodes[1:]:  # node 0 is the root
         component = create_component(n)
         bom.components.add(component)
-        bom.register_dependency(root_component, [component])
+        component_per_node[n] = component
+    for dep in deps_graph.nodes:
+        for dep_dep in dep.dependencies:
+            bom.register_dependency(component_per_node[dep], [component_per_node[dep_dep.dst]])
     serialized_json = JsonV1Dot4(bom).output_as_string()
     cli_out_write(serialized_json)
