ebb5737763da9f89149ee7fa5756da2aa2529237
Jan Kowalleck <jan.kowalleck@gmail.com>
2023-08-14 10:45:01 +0200

    restructured for maintenance
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- extensions/commands/recipe/cmd_create_sbom.py
+++ extensions/commands/recipe/cmd_create_sbom.py
@@ -1,122 +1,116 @@
 import os.path
 import sys
-from typing import Iterable, Optional, Tuple, TYPE_CHECKING, Union
+from typing import TYPE_CHECKING, Iterable, List, Optional, Set, Tuple, Union
 
 from conan.api.conan_api import ConanAPI
-from conan.api.output import cli_out_write, ConanOutput
+from conan.api.output import ConanOutput, cli_out_write
 from conan.cli.args import common_graph_args, validate_common_graph_args
 from conan.cli.command import conan_command
 
 if TYPE_CHECKING:
-    from conans.client.graph.graph import Node
+    from cyclonedx.model.bom import Bom
 
 
-def package_type_to_component_type(pt: str):
-    from cyclonedx.model.component import ComponentType
-    if pt is "application":
-        return ComponentType.APPLICATION
-    else:
-        return ComponentType.LIBRARY
-
-
-def licenses(ids: Union[Tuple[str], str]) -> Optional[Iterable]:
-    """
-    see https://cyclonedx.org/docs/1.4/json/#components_items_licenses
-    """
-    from cyclonedx.factory.license import LicenseFactory
-    from cyclonedx.model import LicenseChoice
-    if ids is None:
-        return None
-    if not isinstance(ids, tuple):
-        ids = [ids]
-    return [LicenseChoice(license=LicenseFactory().make_from_string(i)) for i in ids]
-
-
-def name(n: 'Node') -> str:
-    if n.name:
-        return n.name
-    else:
-        assert name.unknown_name_created is False, "multiple nodes have no name"
-        name.unknown_name_created = True
-        return "UNKNOWN"
-
-
-def package_url(node: 'Node', cached_name: str):
-    """
-    Creates a PURL following https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst#conan
-    """
-    from packageurl import PackageURL
-    return PackageURL(
-        type="conan",
-        name=cached_name,
-        version=node.conanfile.version,
-        qualifiers={
-            "prev": node.prev,
-            "rref": node.ref.revision if node.ref else None,
-            "user": node.conanfile.user,
-            "channel": node.conanfile.channel,
-            "repository_url": node.remote.url if node.remote else None
-        })
-
-
-def create_component(n: 'Node'):
-    from cyclonedx.model.component import Component
-    from cyclonedx.model import ExternalReference, ExternalReferenceType, XsUri
-    name_ = name(n)
-    purl = package_url(n, name_)
-    result = Component(
-        type=package_type_to_component_type(n.conanfile.package_type),
-        name=name_,
-        version=n.conanfile.version,
-        licenses=licenses(n.conanfile.license),
-        bom_ref=purl.to_string(),
-        purl=purl,
-        description=n.conanfile.description
-    )
-    if n.conanfile.homepage:
-        result.external_references.add(ExternalReference(
-            type=ExternalReferenceType.WEBSITE,
-            url=XsUri(n.conanfile.homepage),
-        ))
-    return result
-
-
-def format_cyclonedx_14_json(result):
+def format_cyclonedx_14_json(bom: 'Bom') -> None:
     from cyclonedx.output.json import JsonV1Dot4
-    serialized_json = JsonV1Dot4(result).output_as_string()
+    serialized_json = JsonV1Dot4(bom).output_as_string()
     cli_out_write(serialized_json)
 
 
-def format_text(result):
+def format_text():
     out = ConanOutput()
     out.error("Format 'text' not supported")
-
-
-def me_as_tool():
-    from cyclonedx.model import ExternalReference, ExternalReferenceType, XsUri
-    from cyclonedx.model.bom import Tool
-    result = Tool(name="conan extension recipe:create-sbom")
-    result.external_references.add(ExternalReference(
-        type=ExternalReferenceType.WEBSITE,
-        url=XsUri("https://github.com/conan-io/conan-extensions")))
-    return result
 
 
 @conan_command(group="Recipe", formatters={
     "text": format_text,  # added by default in BaseConanCommand.__init__
     "cyclonedx_1.4_json": format_cyclonedx_14_json})
-def create_sbom(conan_api: ConanAPI, parser, *args):
-    """
-    creates an SBOM in CycloneDX 1.4 JSON format
-    """
+def create_sbom(conan_api: ConanAPI, parser, *args) -> 'Bom':
+    """Create a CycloneDX Software Bill of Materials (SBOM)"""
+
     try:
-        import cyclonedx
+        from cyclonedx.factory.license import LicenseFactory
+        from cyclonedx.model import (ExternalReference, ExternalReferenceType,
+                                     LicenseChoice, Tool, XsUri)
+        from cyclonedx.model.bom import Bom
+        from cyclonedx.model.component import Component, ComponentType
+        from packageurl import PackageURL
     except ModuleNotFoundError:
-        sys.stderr.write(
-            "The sbom extension needs an additional package, please run 'pip install cyclonedx-python-lib'\n")
+        # Assert on RUNTIME of the actual conan-command, that all requirements exist.
+        # Since conan loads all extensions when started, this check could prevent conan from running,
+        # if loading dependencies is performed outside the actual conan-command in global/module scope.
+        print('The sbom extension needs an additional package, please run:'
+              # keep in synk with the instructions in `README.md`
+              "pip install 'cyclonedx-python-lib>=4.0.1,<5.0.0'"
+              , sep='\n', file=sys.stderr)
         sys.exit(1)
-    from cyclonedx.model.bom import Bom
-    # BEGIN COPY FROM conan: cli/commands/graph.py
+
+    if TYPE_CHECKING:
+        from conans.client.graph.graph import Node
+
+    def package_type_to_component_type(pt: str) -> ComponentType:
+        return ComponentType.APPLICATION if pt == "application" else ComponentType.LIBRARY
+
+    def licenses(ls: Optional[Union[Tuple[str, ...], Set[str], List[str], str]]) -> Optional[Iterable[LicenseChoice]]:
+        """
+        see https://cyclonedx.org/docs/1.4/json/#components_items_licenses
+        """
+        if ls is None:
+            return None
+        if not isinstance(ls, (tuple, set, list)):
+            ls = [ls]
+        return [LicenseChoice(license=LicenseFactory().make_from_string(i)) for i in ls]
+
+    def name(node: 'Node') -> str:
+        if node.name:
+            return node.name
+        assert name.unknown_name_created is False, "multiple nodes have no name"
+        name.unknown_name_created = True
+        return "UNKNOWN"
+
+    def package_url(node: 'Node', cached_name: str) -> PackageURL:
+        """
+        Creates a PURL following https://github.com/package-url/purl-spec/blob/master/PURL-TYPES.rst#conan
+        """
+        return PackageURL(
+            type="conan",
+            name=cached_name,
+            version=node.conanfile.version,
+            qualifiers={
+                "prev": node.prev,
+                "rref": node.ref.revision if node.ref else None,
+                "user": node.conanfile.user,
+                "channel": node.conanfile.channel,
+                "repository_url": node.remote.url if node.remote else None
+            })
+
+    def create_component(n: 'Node') -> Component:
+        name_ = name(n)
+        purl = package_url(n, name_)
+        result = Component(
+            type=package_type_to_component_type(n.conanfile.package_type),
+            name=name_,
+            version=n.conanfile.version,
+            licenses=licenses(n.conanfile.license),
+            bom_ref=purl.to_string(),
+            purl=purl,
+            description=n.conanfile.description
+        )
+        if n.conanfile.homepage:
+            result.external_references.add(ExternalReference(
+                type=ExternalReferenceType.WEBSITE,
+                url=XsUri(n.conanfile.homepage),
+            ))
+        return result
+
+    def me_as_tool() -> Tool:
+        tool = Tool(name="conan extension recipe:create-sbom")
+        tool.external_references.add(ExternalReference(
+            type=ExternalReferenceType.WEBSITE,
+            url=XsUri("https://github.com/conan-io/conan-extensions")))
+        return tool
+
+    # region COPY FROM conan: cli/commands/graph.py
     common_graph_args(parser)
     args = parser.parse_args(*args)
     validate_common_graph_args(args)
@@ -139,7 +133,8 @@ def create_sbom(conan_api: ConanAPI, parser, *args):
         deps_graph = conan_api.graph.load_graph_requires(args.requires, args.tool_requires,
                                                          profile_host, profile_build, lockfile,
                                                          remotes, args.update)
-    # END COPY
+    # endregion COPY
+
     name.unknown_name_created = False
     components = {n: create_component(n) for n in deps_graph.nodes}
     bom = Bom()
