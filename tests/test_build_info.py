18ffa29ce9b6164e293d9b9543d7fd1bb38bf593
danimtb <danimanzaneque@gmail.com>
2025-01-14 17:55:18 +0100

    change lib names

--- tests/test_build_info.py
+++ tests/test_build_info.py
@@ -43,50 +43,51 @@ def test_static_library_skip_binaries():
     """
     Test skip binaries behavior with static and shared libraries
     """
-    run("conan new cmake_lib -d name=liba -d version=1.0")
+    run("conan new cmake_lib -d name=lib1 -d version=1.0")
     conanfile = load("conanfile.py")
     conanfile = conanfile.replace('package_type = "library"', 'package_type = "static-library"')
     save("conanfile.py", conanfile)
     run("conan create .")
 
-    run("conan new cmake_lib -d name=libb -d version=1.0 -d requires=liba/1.0 --force")
+    run("conan new cmake_lib -d name=lib2 -d version=1.0 -d requires=lib1/1.0 --force")
     conanfile = load("conanfile.py")
     conanfile = conanfile.replace('package_type = "library"', 'package_type = "shared-library"')
+    #conanfile = conanfile.replace('self.requires("liba/1.0")', 'self.requires("liba/1.0", transitive_libs=True)')
     save("conanfile.py", conanfile)
-    run("conan create . -o libb/*:shared=True")
+    run("conan create . -o lib2/*:shared=True")
 
-    run("conan new cmake_lib -d name=libc -d version=1.0 -d requires=libb/1.0 --force")
+    run("conan new cmake_lib -d name=lib3 -d version=1.0 -d requires=lib2/1.0 --force")
 
-    run("conan create . -o libb/1.0:shared=True -f json > create.json")
+    run("conan create . -o lib2/1.0:shared=True -f json > create.json")
     graph = json.loads(load("create.json"))["graph"]
     assert graph["nodes"]["3"]["binary"] == "Skip"
     assert graph["nodes"]["3"]["package_folder"] is None
 
     out = run("conan art:build-info create create.json build_name 1 repo --with-dependencies > bi.json")
-    assert "WARN: Package marked as 'Skip' for liba/1.0" in out
+    assert "WARN: Package marked as 'Skip' for lib1/1.0" in out
     build_info = json.loads(load("bi.json"))
-    assert "libc/1.0" in build_info["modules"][1]["id"].split(":")[0]  # libc package
+    assert "lib3/1.0" in build_info["modules"][1]["id"].split(":")[0]  # libc package
     # Remove package id and rrev leave them as "<name>/<version> :: <file>" to make the asserts platform agnostic
     dependencies_ids = [f"{dep['id'].split('#')[0]} ::{dep['id'].split('::')[-1]}" for dep in build_info["modules"][1]["dependencies"]]
     assert len(dependencies_ids) == 2  # liba package files are not present as binary is skipped
-    assert "libb/1.0 :: conaninfo.txt" in dependencies_ids
-    assert "libb/1.0 :: conanmanifest.txt" in dependencies_ids
+    assert "lib2/1.0 :: conaninfo.txt" in dependencies_ids
+    assert "lib2/1.0 :: conanmanifest.txt" in dependencies_ids
 
-    run("conan create . -o libb/1.0:shared=True -c tools.graph:skip_binaries=False -f json > create.json")
+    run("conan create . -o lib2/1.0:shared=True -c tools.graph:skip_binaries=False -f json > create.json")
     graph = json.loads(load("create.json"))["graph"]
     assert graph["nodes"]["3"]["binary"] == "Cache"
     assert graph["nodes"]["3"]["package_folder"] is not None
     out = run("conan art:build-info create create.json build_name 1 repo --with-dependencies > bi.json")
-    assert "WARN: Package marked as 'Skip' for liba/1.0" not in out
+    assert "WARN: Package marked as 'Skip' for lib1/1.0" not in out
     build_info = json.loads(load("bi.json"))
-    assert "libc/1.0#95c7f81c13006116d5f1abc3f58af7f8" in build_info["modules"][1]["id"].split(":")[0]  # libc package
+    assert "lib3/1.0" in build_info["modules"][1]["id"].split(":")[0]  # libc package
     # Remove package id and rrev leave them as "<name>/<version>#rrev :: <file>" to make the asserts platform agnostic
     dependencies_ids = [f"{dep['id'].split('#')[0]} ::{dep['id'].split('::')[-1]}" for dep in build_info["modules"][1]["dependencies"]]
     assert len(dependencies_ids) == 4  # liba package files now should be present
-    assert "libb/1.0 :: conaninfo.txt" in dependencies_ids
-    assert "libb/1.0 :: conanmanifest.txt" in dependencies_ids
-    assert "liba/1.0 :: conaninfo.txt" in dependencies_ids
-    assert "liba/1.0 :: conanmanifest.txt" in dependencies_ids
+    assert "lib2/1.0 :: conaninfo.txt" in dependencies_ids
+    assert "lib2/1.0 :: conanmanifest.txt" in dependencies_ids
+    assert "lib1/1.0 :: conaninfo.txt" in dependencies_ids
+    assert "lib1/1.0 :: conanmanifest.txt" in dependencies_ids
 
 
 def test_tool_require_skip_binaries():
