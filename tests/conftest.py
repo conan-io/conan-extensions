d369e001c74e727ef9fff36c23b2389ce202185b
Abril Rincón Blanco <abrilrb@jfrog.com>
2025-01-20 17:56:46 +0100

    Add timestamp tests for art:promote

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,15 +7,13 @@ import pytest
 def pytest_collection_modifyitems(items):
     for item in items:
         if "requires_credentials" in item.keywords:
-            if (
-                not os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_PROD")
-                or not os.getenv("CONAN_PASSWORD_EXTENSIONS_PROD")
-                or not os.getenv("CONAN_LOGIN_USERNAME_EXTENSIONS_STG")
-                or not os.getenv("CONAN_PASSWORD_EXTENSIONS_STG")
-                or not os.getenv("ART_URL")
-            ):
-                item.add_marker(pytest.mark.skip(reason="Missing required credentials environment variables"))
-                print(f"Skipping test {item.nodeid}. Missing required credentials environment variables.")
+            for env in ["CONAN_LOGIN_USERNAME_EXTENSIONS_PROD", "CONAN_PASSWORD_EXTENSIONS_PROD",
+                        "CONAN_LOGIN_USERNAME_EXTENSIONS_STG", "CONAN_PASSWORD_EXTENSIONS_STG",
+                        "ART_URL"]:
+                if not os.getenv(env):
+                    item.add_marker(pytest.mark.skip(reason=f"Missing required credentials environment variable {env}."))
+                    print(f"Skipping test {item.nodeid}. Missing required credentials environment variable {env}")
+                    break
 
 ALL_OS = set("darwin linux win32".split())
 
