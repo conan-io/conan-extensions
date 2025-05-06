7422f57835b1f65c16b7287aa9f54cc323fce0db
Ernesto de Gracia Herranz <ernestodgh@jfrog.com>
2025-05-06 17:09:12 +0200

    it works!
    Co-Authored-By: Abril Rincón Blanco <5364255+AbrilRBS@users.noreply.github.com>

--- extensions/commands/diff/cmd_check_diff.py
+++ extensions/commands/diff/cmd_check_diff.py
@@ -7,6 +7,7 @@ from conan.api.conan_api import ConanAPI
 from conan.api.output import ConanOutput
 from conan.cli.command import conan_command
 from conan.internal.util.files import chdir
+from conan.api.model import RecipeReference
 
 def output_json(msg):
     return json.dumps({"greet": msg})
@@ -17,50 +18,71 @@ def check_diff(conan_api: ConanAPI, parser, *args):
     """
     Command to get the diff between versions
     """
-    parser.add_argument("path", help="Path to the conanfile")
-    parser.add_argument("-o", "--old", help="Old version")
-    parser.add_argument("-n", "--new", help="New version")
+
+    parser.add_argument("-op", "--old-path", help="Path to the old recipe")
+    parser.add_argument("-ov", "--old-reference", help='Old reference "mylib/1.0"')
+    parser.add_argument("-or", "--old-require", help='Old reference "mylib/1.0"')
+
+    parser.add_argument("-np", "--new-path", help="Path to the new recipe")
+    parser.add_argument("-nv", "--new-reference", help="New reference")
+    parser.add_argument("-nr", "--new-require", help='New reference "mylib/1.0"')
+
     parser.add_argument("-s", "--split_diff", action="store_true")
     parser.add_argument("--encoding", default="utf-8", help="Encoding to read diff")
+    parser.add_argument("-r", "--remote", action="append", default=None,
+                       help='Look in the specified remote or remotes server')
+
     args = parser.parse_args(*args)
 
     cwd = os.getcwd()
-    path = conan_api.local.get_conanfile_path(args.path, cwd, py=True)
-    enabled_remotes = conan_api.remotes.list()
-    if not os.path.exists(f"diff{args.old}-{args.new}.patch"):
-        t = tempfile.mkdtemp(suffix='conans')
+    enabled_remotes = conan_api.remotes.list(args.remote or "*")
 
-        old_folder = os.path.join(t, "old")
-        new_folder = os.path.join(t, "new")
-        os.mkdir(old_folder)
-        os.mkdir(new_folder)
+    def _download_ref_from_remote(reference):
+        ref = RecipeReference().loads(reference)
+        conan_api.download.recipe(ref, enabled_remotes[0])
+        cache_path = conan_api.cache.export_path(ref)
+        return ref, cache_path
 
-        ConanOutput().info("old_folder: " + old_folder)
-        ConanOutput().info("new_folder: " + new_folder)
+    def _export_recipe_from_path(path_to_conanfile, reference):
+        path = conan_api.local.get_conanfile_path(path_to_conanfile, cwd, py=True)
+        ref = RecipeReference().loads(reference)
+        export_ref, conanfile = conan_api.export.export(path=path,
+                                                        name=ref.name, version=ref.version,
+                                                        user=ref.user, channel=ref.channel,
+                                                        lockfile=None,
+                                                        remotes=enabled_remotes)
+        cache_path = conan_api.cache.export_path(export_ref)
+        return export_ref, cache_path
 
-        with chdir(old_folder):
-            conan_api.local.source(path, version=args.old, remotes=enabled_remotes)
+    def _source(path_to_conanfile, reference, required_ref):
+        if required_ref is not None:
+            export_ref, cache_path = _download_ref_from_remote(required_ref)
+        else:
+            export_ref, cache_path = _export_recipe_from_path(path_to_conanfile, reference)
+        exported_path = conan_api.local.get_conanfile_path(cache_path, cwd, py=True)
+        conan_api.local.source(exported_path, name=export_ref.name, version=str(export_ref.version), user=export_ref.user,
+                               channel=export_ref.channel, remotes=enabled_remotes)
+        return export_ref, cache_path
 
-        with chdir(new_folder):
-            conan_api.local.source(path, version=args.new, remotes=enabled_remotes)
+    old_export_ref, old_cache_path = _source(args.old_path, args.old_reference, args.old_require)
+    new_export_ref, new_cache_path = _source(args.new_path, args.new_reference, args.new_require)
 
-        with open(f"diff{args.old}-{args.new}.patch", "w") as f:
-            subprocess.run(["git", "diff", old_folder, new_folder], stdout=f, check=True)
 
-        ConanOutput().info(f"diff in diff{args.old}-{args.new}.patch")
+    with open(f"diff{old_export_ref.version}-{new_export_ref.version}.patch", "w") as f:
+        subprocess.run(["git", "diff", old_cache_path, new_cache_path], stdout=f, check=False)
 
-    with open(f"diff{args.old}-{args.new}.patch", "r", encoding=args.encoding) as file:
+    with open(f"diff{old_export_ref.version}-{new_export_ref.version}.patch", "r", encoding=args.encoding) as file:
         diff_text = file.read()
         ConanOutput().info(f"diff ready")
-    name = f"{args.old}-{args.new}"
+    name = f"{old_export_ref.version}-{new_export_ref.version}"
 
     if args.split_diff:
-        diff_to_multi_html(name, diff_text)
+        diff_to_multi_html(name, diff_text, old_cache_path[1:], new_cache_path[1:])
     else:
-        diff_to_single_html(name, diff_text)
-    subprocess.run(["open", os.path.join(cwd, f"diff_{args.old}-{args.new}.html")])
+        diff_to_single_html(name, diff_text, old_cache_path[1:], new_cache_path[1:])
+    subprocess.run(["open", os.path.join(cwd, f"diff_{old_export_ref.version}-{new_export_ref.version}.html")])
 
-def diff_to_multi_html(name, diff_text):
+def diff_to_multi_html(name, diff_text, old_cache_path, new_cache_path):
     file_diffs = {}
     current_file = None
     current_lines = []
@@ -129,7 +151,7 @@ def diff_to_multi_html(name, diff_text):
         # Table of content
         for file_name in file_diffs:
             safe_name = file_name.replace('/', '_')
-            f.write(f'<li><a href="{output_dir}/{safe_name}.html" target="diff-frame">{file_name}</a></li>\n')
+            f.write(f'<li><a href="{output_dir}/{safe_name}.html" target="diff-frame">{file_name.replace(old_cache_path, "(old)").replace(new_cache_path, "(new)")}</a></li>\n')
 
         f.write("""
                 </ul>
@@ -141,7 +163,7 @@ def diff_to_multi_html(name, diff_text):
         """)
 
 
-def diff_to_single_html(name, diff_text):
+def diff_to_single_html(name, diff_text, old_cache_path, new_cache_path):
     html_lines = [
         f"<html><head><title>{name}</title><style>",
         ".container { display: flex; height: 100%; }",
@@ -159,7 +181,7 @@ def diff_to_single_html(name, diff_text):
 
     html_lines.append(f"<div class='container'><div class='sidebar'><h2>{name}</h2><h2>File list:</h2><ul>")
     for file in sorted(file_names):
-        html_lines.append(f'<li><a href="#{file}">{file}</a></li>')
+        html_lines.append(f'<li><a href="#{file}">{file.replace(old_cache_path, "(old)").replace(new_cache_path, "(new)")}</a></li>')
     html_lines.append("</ul></div><div class='content'>")
 
     for line in diff_text.splitlines():
